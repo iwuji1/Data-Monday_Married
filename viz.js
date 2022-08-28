fd9bcb8014a0952b90f3e4656bf70ec5759fc4da
Obinna Iwuiji <obiwuji@Obinnas-MacBook-Pro.local>
2022-08-28 18:13:24 +0100

    fixed broken tooltip

--- viz.js
+++ viz.js
@@ -56,13 +56,15 @@ d3.csv("never_married_clean.csv", function(error, data) {
   var z = d3.scaleOrdinal(d3.schemeCategory10)
             .domain(years);
 
-  console.log(male_dat, width, width/2)
+  // console.log(male_dat, width, width/2)
   //appending the rectangles for the bar chart for males
 
   var tooltip = d3.select("#my_dataviz")
       .append("div")
-      .style("opacity", 0)
+      .style('visibility', 'hidden')
       .attr("class", "tooltip")
+      .style("position", "absolute")
+      .style("z-index", "10")
       .style("background-color", "white")
       .style("border", "solid")
       .style("border-width", "1px")
@@ -72,23 +74,20 @@ d3.csv("never_married_clean.csv", function(error, data) {
     // A function that change this tooltip when the user hover a point.
     // Its opacity is set to 1: we can now see it. Plus it set the text and position of tooltip depending on the datapoint (d)
   var mouseover = function(d) {
-    tooltip
-      .style("opacity", 1)
+    tooltip.style('visibility', 'visible');
+    d3.select(this).transition().style("stroke", "00008B")
   }
 
   var mousemove = function(d) {
     tooltip
-      .html("Year: " + d.key + "<br/>" +
-            " Percentage never married: " + d.value)
-      .style("left", (d3.mouse(this)[0]+100) + "px") // It is important to put the +90: other wise the tooltip is exactly where the point is an it creates a weird effect
-      .style("top", (d3.mouse(this)[1]+280) + "px")
+      .html(`<div>Year: ${d.key}</div><div>Percentage never married: ${d.value}</div>`)
+            .style('top', d3.event.pageY - 10 + 'px')
+            .style('left', d3.event.pageX + 10 + 'px');
   }
   // A function that change this tooltip when the leaves a point: just need to set opacity to 0 again
   var mouseleave = function(d) {
-    tooltip
-      .transition()
-      .duration(200)
-      .style("opacity", 0)
+    tooltip.html(``).style('visibility', 'hidden');
+    d3.select(this).transition().style("stroke-opacity", 0)
   }
 
   svg.append("g")
@@ -98,18 +97,31 @@ d3.csv("never_married_clean.csv", function(error, data) {
       .append("g")
         .attr("class","g")
         .attr("transform", function(d) {return "translate( 0," + yscale(d.age_range) + ")";})
+        // .text(function(d) {return d.age_range})
       .selectAll("rect")
       .data(function(d) {return years.map(function(key) {return {key:key, value: d[key]}; }); })
       .enter().append("rect")
-        .attr("class", function(d) {return console.log(d)})
         .attr("x", function(d) {return xscale_m(d.value);})
         .attr("y", function(d) {return year_scale(d.key);})
         .attr("width", function(d) { return (width/2) - xscale_m(d.value);})
         .attr("height", year_scale.bandwidth)
         .attr("fill", function(d) {return z(d.key)})
-      .on("mouseover", mouseover )
-      .on("mousemove", mousemove )
-      .on("mouseleave", mouseleave );
+      .on("mouseover", function (d,i) {
+        tooltip
+          .html(`<div>Year: ${d.key}</div><div>Percentage never married: ${Math.round(d.value*100) + "%"}</div>`)
+          .style('visibility', 'visible');
+          d3.select(this).transition().attr("stroke","#FFFFFF")
+                                      .attr("stroke-width", 2+"px");
+      })
+      .on("mousemove", function() {
+        tooltip
+            .style('top', d3.event.pageY - 10 + 'px')
+            .style('left', d3.event.pageX + 10 + 'px');
+      })
+      .on("mouseleave", function() {
+        tooltip.html(``).style('visibility', 'hidden');
+        d3.select(this).transition().attr("stroke","none");
+      });
 
   //appending the rectangles for the bar chart for females
 
@@ -123,16 +135,28 @@ d3.csv("never_married_clean.csv", function(error, data) {
       .selectAll("rect")
       .data(function(d) {return years.map(function(key) {return {key:key, value: d[key]}; }); })
       .enter().append("rect")
-        .attr("class", function(d) {return console.log(d)})
         .attr("x", function(d) {return (d.value);})
         .attr("y", function(d) {return year_scale(d.key);})
         .attr("width", function(d) { return (width/2) - xscale_f(d.value);})
         .attr("height", year_scale.bandwidth)
         .attr("fill", function(d) {return z(d.key)})
         .attr("transform", "translate(" + width/2 + ",0)")
-      .on("mouseover", mouseover )
-      .on("mousemove", mousemove )
-      .on("mouseleave", mouseleave );
+        .on("mouseover", function (d,i) {
+          tooltip
+            .html(`<div>Year: ${d.key}</div><div>Percentage never married: ${Math.round(d.value*100) + "%"}</div>`)
+            .style('visibility', 'visible');
+            d3.select(this).transition().attr("stroke","#FFFFFF")
+                                        .attr("stroke-width", 2+"px");
+        })
+        .on("mousemove", function() {
+          tooltip
+              .style('top', d3.event.pageY - 10 + 'px')
+              .style('left', d3.event.pageX + 10 + 'px');
+        })
+        .on("mouseleave", function() {
+          tooltip.html(``).style('visibility', 'hidden');
+          d3.select(this).transition().attr("stroke","none");
+        });
 
   svg.append("text")
        .attr("transform",
@@ -154,6 +178,13 @@ svg.append("text")
                           (height/2) + "), rotate(90)")
      .style("text-anchor", "middle")
      .text("Female");
+
+svg.append("g")
+  .attr("class", "central")
+  .attr("transform",
+        "translate(" + ((width/2)+10) + " ," +(-65)+ ")")
+  .style("text-anchor", "middle")
+  .call(d3.axisLeft(yscale))
 
 
 })
